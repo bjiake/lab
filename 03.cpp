5421b49c37a0509f7d2767b4515e01c91303b5ba
bjiake <suyd@list.ru>
2021-12-21 14:56:39 +0300

    th

--- 03.cpp
+++ 03.cpp
@@ -6,10 +6,12 @@
 
 using namespace std;
 
-int countMonth(int startDay, int daysNumber, const char* name, int week) {
+int countMonth(int startDay, int daysNumber, const char* name) {
+    int week = 7;
+
     cout << "                     " << name << endl
-        << "__________________________________________________" << endl
-        << "Пн\tВт\tСр\tЧт\tПт\tСб\tВс" << endl;
+         << "__________________________________________________" << endl
+         << "Пн\tВт\tСр\tЧт\tПт\tСб\tВс" << endl;
 
     for (int index = 1; index < startDay; ++index) {//Начало в нужной неделе
         cout << "\t";
@@ -33,45 +35,43 @@ int countMonth(int startDay, int daysNumber, const char* name, int week) {
 
 int main() {
     setlocale(LC_ALL, "ru");
-    int a, Feb;
+    int numberDay, February;
     int week = 7, year = 1900, highYear = 366, lowYear = 365;
     cout << "Введите год: ";
-    cin >> a;
-    if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0) {//високосный год
-        Feb = 29;
-    }
-    else {
-        Feb = 28;
+    cin >> numberDay;
+    if ((numberDay % 4 == 0 && numberDay % 100 != 0) || numberDay % 400 == 0) {//високосный год
+        February = 29;
+    } else {
+        February = 28;
     }
 
-    int leapyears, years, days, b;
+    int leapYears, years, days, b;
 
     years = a - year;
-    leapyears = (years - 1) / 4;
+    leapYears = (years - 1) / 4;
 
-    if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0) {//високосный год
-        days = years * highYear + leapyears;
+    if ((numberDay % 4 == 0 && numberDay % 100 != 0) || numberDay % 400 == 0) {//високосный год
+        days = years * highYear + leapYears;
     }
     else {
-        days = years * lowYear + leapyears;
+        days = years * lowYear + leapYears;
     }
 
     b = days % week;
-
     cout << endl;
 
-    b = countMonth(b, 31, "Январь", week);
-    b = countMonth(b, Feb, "Февраль", week);
-    b = countMonth(b, 31, "Март", week);
-    b = countMonth(b, 30, "Апрель", week);
-    b = countMonth(b, 31, "Май", week);
-    b = countMonth(b, 30, "Июнь", week);
-    b = countMonth(b, 31, "Июль", week);
-    b = countMonth(b, 31, "Август", week);
-    b = countMonth(b, 30, "Сентябрь", week);
-    b = countMonth(b, 31, "Октябрь", week);
-    b = countMonth(b, 30, "Ноябрь", week);
-    b = countMonth(b, 31, "Декабрь", week);
+    b = countMonth(b, 31, "Январь");
+    b = countMonth(b, Feb, "Февраль");
+    b = countMonth(b, 31, "Март");
+    b = countMonth(b, 30, "Апрель");
+    b = countMonth(b, 31, "Май");
+    b = countMonth(b, 30, "Июнь");
+    b = countMonth(b, 31, "Июль");
+    b = countMonth(b, 31, "Август");
+    b = countMonth(b, 30, "Сентябрь");
+    b = countMonth(b, 31, "Октябрь");
+    b = countMonth(b, 30, "Ноябрь");
+    b = countMonth(b, 31, "Декабрь");
     system("pause");
 }
 
